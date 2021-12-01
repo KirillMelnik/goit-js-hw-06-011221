ac7654a1c1e3d157d9b7d4e64a9124be6624e9e8
Kirill Melnik <k.melnik@letyshops.com>
2021-12-02 00:29:08 +0200

    1 дз

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,1 +1,7 @@
-const ffdsf = 5; 
+const list = document.querySelectorAll('.item')
+console.log(`Number of categories: ${list.length}`)
+
+list.forEach(item => {
+    console.log(`Category: ${item.querySelector('h2').textContent}`);
+    console.log(`Elements: ${item.querySelectorAll('li').length}`)
+})
